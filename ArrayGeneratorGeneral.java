62918ca79868cda3fea62f671cd6737711336159
Raffaele <raffaele.perri@usi.ch>
2023-11-16 14:41:01 +0100

    final results

--- ArrayGeneratorGeneral.java
+++ ArrayGeneratorGeneral.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
@@ -5,167 +6,141 @@ import java.util.Arrays;
 public class ArrayGeneratorGeneral {
 
     public static void main(String[] args) {
-        int dimension = 1000;
+        int dimension = 10000;
 
         BubbleSortPassPerItem bubbleSortPassPerItem = new BubbleSortPassPerItem();
         BubbleSortUntilNoChange bubbleSortUntilNoChange = new BubbleSortUntilNoChange();
-        BubbleSortWhileNeeded bubbleSortWhileNeeded = new BubbleSortWhileNeeded();   
-        
+        BubbleSortWhileNeeded bubbleSortWhileNeeded = new BubbleSortWhileNeeded();
 
-        String[] typeOfTheArray = {"random", "sorted", "partSorted", "reverse", "duplicates", "noDuplicates", "equal"};
+        String[] typeOfTheArray = { "random", "sorted", "partSorted", "reverse", "duplicates", "noDuplicates",
+                "equal" };
 
-        //FOR INT
+        // FOR INT
         for (String type : typeOfTheArray) {
-            int[] intArray = (int[]) arrayGeneratorGeneral("int", dimension, type);
-            // Convert int[] to Integer[]
-            Integer[] integerArray = Arrays.stream(intArray).boxed().toArray(Integer[]::new);
-            Integer[] integerArrayCopy = Arrays.copyOf(integerArray, integerArray.length);
-            Integer[] integerArrayCopy2 = Arrays.copyOf(integerArray, integerArray.length);
-            
             for (int i = 0; i < 150; i++) {
-                //int
-                analyzeSortingPerformance(bubbleSortPassPerItem, integerArray, "bubbleSortPassPerItem", "int", type, i);
-            }
-            System.out.println(Arrays.toString(integerArrayCopy));
-            for (int i = 0; i < 150; i++) {
-                analyzeSortingPerformance(bubbleSortUntilNoChange, integerArrayCopy, "bubbleSortUntilNoChange", "int", type, i);
-            }
-            for (int i = 0; i < 150; i++) {
-                analyzeSortingPerformance(bubbleSortWhileNeeded, integerArrayCopy2, "bubbleSortWhileNeeded", "int", type, i);
+                // int
+                int[] intArray = (int[]) arrayGeneratorGeneral("int", dimension, type);
+                // Convert int[] to Integer[]
+                Integer[] integerArray = Arrays.stream(intArray).boxed().toArray(Integer[]::new);
+                Integer[] integerArrayCopy = Arrays.copyOf(integerArray, dimension);
+                Integer[] integerArrayCopy2 = Arrays.copyOf(integerArray, dimension);
+
+                analyzeSortingPerformance(bubbleSortPassPerItem, integerArray, "PassPerItem", "int", type, i);
+                analyzeSortingPerformance(bubbleSortUntilNoChange, integerArrayCopy, "UntilNoChange", "int", type, i);
+                analyzeSortingPerformance(bubbleSortWhileNeeded, integerArrayCopy2, "WhileNeeded", "int", type, i);
             }
 
         }
 
         // FOR INTEGER
         for (String type : typeOfTheArray) {
-            Integer[] integerArray = (Integer[]) arrayGeneratorGeneral("Integer", dimension, type);
-            Integer[] integerArrayCopy = integerArray;
-            Integer[] integerArrayCopy2 = integerArray;
 
-            // for (int i = 0; i < 150; i++) {
-            //     //Integer
-            //     analyzeSortingPerformance(bubbleSortPassPerItem, integerArray, "bubbleSortPassPerItem", "Integer", type, i);
-            // }
-            // for (int i = 0; i < 150; i++) {
-            //     analyzeSortingPerformance(bubbleSortUntilNoChange, integerArrayCopy, "bubbleSortUntilNoChange", "Integer", type, i);
-            // }
-            // for (int i = 0; i < 150; i++) {
-            //     analyzeSortingPerformance(bubbleSortWhileNeeded, integerArrayCopy2, "bubbleSortWhileNeeded", "Integer", type, i);
-            // }
+            for (int i = 0; i < 150; i++) {
+                // Integer
+                Integer[] integerArray = (Integer[]) arrayGeneratorGeneral("Integer", dimension, type);
+                Integer[] integerArrayCopy = Arrays.copyOf(integerArray, dimension);
+                Integer[] integerArrayCopy2 = Arrays.copyOf(integerArray, dimension);
+
+                analyzeSortingPerformance(bubbleSortPassPerItem, integerArray, "PassPerItem", "Integer", type, i);
+                analyzeSortingPerformance(bubbleSortUntilNoChange, integerArrayCopy, "UntilNoChange", "Integer", type,
+                        i);
+                analyzeSortingPerformance(bubbleSortWhileNeeded, integerArrayCopy2, "WhileNeeded", "Integer", type, i);
+            }
         }
 
         // FOR STRING
         for (String type : typeOfTheArray) {
-            String[] stringArray = (String[]) arrayGeneratorGeneral("String", dimension, type);
-            String[] stringArrayCopy = stringArray;
-            String[] stringArrayCopy2 = stringArray;
 
-            // for (int i = 0; i < 150; i++) {
-            //     //String
-            //     analyzeSortingPerformance(bubbleSortPassPerItem, stringArray, "bubbleSortPassPerItem", "String", type, i);
-            // }
-            // for (int i = 0; i < 150; i++) {
-            //     analyzeSortingPerformance(bubbleSortUntilNoChange, stringArrayCopy, "bubbleSortUntilNoChange", "String", type, i);
-            // }
-            // for (int i = 0; i < 150; i++) {
-            //     analyzeSortingPerformance(bubbleSortWhileNeeded, stringArrayCopy2, "bubbleSortWhileNeeded", "String", type, i);
-            // }
+            for (int i = 0; i < 150; i++) {
+                // String
+                String[] stringArray = (String[]) arrayGeneratorGeneral("String", dimension, type);
+                String[] stringArrayCopy = Arrays.copyOf(stringArray, dimension);
+                String[] stringArrayCopy2 = Arrays.copyOf(stringArray, dimension);
+
+                analyzeSortingPerformance(bubbleSortPassPerItem, stringArray, "PassPerItem", "String", type, i);
+                analyzeSortingPerformance(bubbleSortUntilNoChange, stringArrayCopy, "UntilNoChange", "String", type, i);
+                analyzeSortingPerformance(bubbleSortWhileNeeded, stringArrayCopy2, "WhileNeeded", "String", type, i);
+            }
         }
 
         // FOR DOUBLE
         for (String type : typeOfTheArray) {
-            double[] doubleArray = (double[]) arrayGeneratorGeneral("double", dimension, type);
-            // Convert double[] to Double[]
-            Double[] doubleArray2 = Arrays.stream(doubleArray).boxed().toArray(Double[]::new);
-            Double[] doubleArrayCopy = Arrays.stream(doubleArray).boxed().toArray(Double[]::new);
-            Double[] doubleArrayCopy2 = Arrays.stream(doubleArray).boxed().toArray(Double[]::new);
-            
-            // for (int i = 0; i < 150; i++) {
-            //     //Double
-            //     analyzeSortingPerformance(bubbleSortPassPerItem, doubleArray2, "bubbleSortPassPerItem", "Double", type, i);
-            // }
-            // for (int i = 0; i < 150; i++) {
-            //     analyzeSortingPerformance(bubbleSortUntilNoChange, doubleArrayCopy, "bubbleSortUntilNoChange", "Double", type, i);
-            // }
-            // for (int i = 0; i < 150; i++) {
-            //     analyzeSortingPerformance(bubbleSortWhileNeeded, doubleArrayCopy2, "bubbleSortWhileNeeded", "Double", type, i);            
-            // }
+
+            for (int i = 0; i < 150; i++) {
+                // Double
+                double[] doubleArray = (double[]) arrayGeneratorGeneral("double", dimension, type);
+                // Convert double[] to Double[]
+                Double[] doubleArray2 = Arrays.stream(doubleArray).boxed().toArray(Double[]::new);
+                Double[] doubleArrayCopy = Arrays.copyOf(doubleArray2, dimension);
+                Double[] doubleArrayCopy2 = Arrays.copyOf(doubleArray2, dimension);
+
+                analyzeSortingPerformance(bubbleSortPassPerItem, doubleArray2, "PassPerItem", "Double", type, i);
+                analyzeSortingPerformance(bubbleSortUntilNoChange, doubleArrayCopy, "UntilNoChange", "Double", type, i);
+                analyzeSortingPerformance(bubbleSortWhileNeeded, doubleArrayCopy2, "WhileNeeded", "Double", type, i);
+            }
         }
 
         // FOR CHAR
         for (String type : typeOfTheArray) {
-            char[] charArray = (char[]) arrayGeneratorGeneral("char", dimension, type);
-            // Convert char[] to Character[]
-            Character[] charArray2 = new Character[charArray.length];
-            Character[] charArrayCopy = new Character[charArray.length];
-            Character[] charArrayCopy2 = new Character[charArray.length];
-            for (int i = 0; i < charArray.length; i++) {
-                charArray2[i] = charArray[i];
+
+            for (int i = 0; i < 150; i++) {
+                // Character
+                char[] charArray = (char[]) arrayGeneratorGeneral("char", dimension, type);
+                // Convert char[] to Character[]
+                Character[] charArray2 = new Character[charArray.length];
+                for (int j = 0; j < charArray.length; j++) {
+                    charArray2[j] = charArray[j];
+                }
+                Character[] charArrayCopy = Arrays.copyOf(charArray2, dimension);
+                Character[] charArrayCopy2 = Arrays.copyOf(charArray2, dimension);
+                analyzeSortingPerformance(bubbleSortPassPerItem, charArray2, "PassPerItem", "Character", type, i);
+                analyzeSortingPerformance(bubbleSortUntilNoChange, charArrayCopy, "UntilNoChange", "Character", type,
+                        i);
+                analyzeSortingPerformance(bubbleSortWhileNeeded, charArrayCopy2, "WhileNeeded", "Character", type, i);
             }
-            for (int i = 0; i < charArray.length; i++) {
-                charArrayCopy[i] = charArray[i];
-            }
-            for (int i = 0; i < charArray.length; i++) {
-                charArrayCopy2[i] = charArray[i];
-            }
-            
-            // for (int i = 0; i < 150; i++) {
-            //     //Character
-            //     analyzeSortingPerformance(bubbleSortPassPerItem, charArray2, "bubbleSortPassPerItem", "Character", type, i);
-            // }
-            // for (int i = 0; i < 150; i++) {
-            //     analyzeSortingPerformance(bubbleSortUntilNoChange, charArrayCopy, "bubbleSortUntilNoChange", "Character", type, i);
-            // }
-            // for (int i = 0; i < 150; i++) {
-            //     analyzeSortingPerformance(bubbleSortWhileNeeded, charArrayCopy2, "bubbleSortWhileNeeded", "Character", type, i);
-            // }
         }
 
         // FOR BYTE
         for (String type : typeOfTheArray) {
-            byte[] byteArray = (byte[]) arrayGeneratorGeneral("byte", dimension, type);
-            // Convert byte[] to Byte[]
-            Byte[] byteArray2 = new Byte[byteArray.length];
-            Byte[] byteArrayCopy = new Byte[byteArray.length];
-            Byte[] byteArrayCopy2 = new Byte[byteArray.length];
 
-            for (int i = 0; i < byteArray.length; i++) {
-                byteArray2[i] = byteArray[i];
-            }
-            for (int i = 0; i < byteArray.length; i++) {
-                byteArrayCopy[i] = byteArray[i];
-            }
-            for (int i = 0; i < byteArray.length; i++) {
-                byteArrayCopy2[i] = byteArray[i];
-            }
+            for (int i = 0; i < 150; i++) {
+                // Byte
+                byte[] byteArray = (byte[]) arrayGeneratorGeneral("byte", dimension, type);
+                // Convert byte[] to Byte[]
+                Byte[] byteArray2 = new Byte[byteArray.length];
+                for (int j = 0; j < byteArray.length; j++) {
+                    byteArray2[j] = byteArray[j];
+                }
+                Byte[] byteArrayCopy = Arrays.copyOf(byteArray2, dimension);
+                Byte[] byteArrayCopy2 = Arrays.copyOf(byteArray2, dimension);
 
-            // for (int i = 0; i < 150; i++) {
-            //     //Byte
-            //     analyzeSortingPerformance(bubbleSortPassPerItem, byteArray2, "bubbleSortPassPerItem", "Byte", type, i);
-            // }
-            // for (int i = 0; i < 150; i++) {
-            //     analyzeSortingPerformance(bubbleSortUntilNoChange, byteArrayCopy, "bubbleSortUntilNoChange", "Byte", type, i);
-            // }
-            // for (int i = 0; i < 150; i++) {
-            //     analyzeSortingPerformance(bubbleSortWhileNeeded, byteArrayCopy2, "bubbleSortWhileNeeded", "Byte", type, i);
-            // }
+                analyzeSortingPerformance(bubbleSortPassPerItem, byteArray2, "PassPerItem", "Byte", type, i);
+                analyzeSortingPerformance(bubbleSortUntilNoChange, byteArrayCopy, "UntilNoChange", "Byte", type, i);
+                analyzeSortingPerformance(bubbleSortWhileNeeded, byteArrayCopy2, "WhileNeeded", "Byte", type, i);
+            }
         }
     }
 
-    public static <T extends Comparable<T>> void analyzeSortingPerformance(Sorter<T> sorter, T[] array, String funcName, String type, String typeOfTheArray, int counter) {
+    public static <T extends Comparable<T>> void analyzeSortingPerformance(Sorter<T> sorter, T[] array, String funcName,
+            String type, String typeOfTheArray, int counter) {
         long startTime = System.nanoTime();
         sorter.sort(array);
         long endTime = System.nanoTime();
         long executionTime = endTime - startTime;
-        
+
         if (counter >= 25) {
             writeResultToCSV(funcName, type, typeOfTheArray, startTime, endTime, executionTime);
         }
     }
 
-    public static void writeResultToCSV(String funcName, String type, String typeOfTheArray, long startTime, long endTime, long executionTime) {
-        String csvFilename = "resultInt1000.csv";
+    public static void writeResultToCSV(String funcName, String type, String typeOfTheArray, long startTime,
+            long endTime, long executionTime) {
+        String csvFilename = "results10000.csv";
         try (FileWriter writer = new FileWriter(csvFilename, true)) {
+            // check if file is empty
+            if (new File(csvFilename).length() == 0) {
+                writer.append("ALGORITHM,ELEMENT ARRAY TYPE,ARRAY ORDER,START TIME,END TIME,ELAPSED TIME\n");
+            }
             writer.append(funcName)
                     .append(",")
                     .append(type)
@@ -177,13 +152,13 @@ public class ArrayGeneratorGeneral {
                     .append(String.valueOf(endTime))
                     .append(",")
                     .append(String.valueOf(executionTime))
-                    .append("\n");                    
+                    .append("\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static Object arrayGeneratorGeneral(String type, int dimension, String typeOfTheArray){
+    public static Object arrayGeneratorGeneral(String type, int dimension, String typeOfTheArray) {
         int choice = 0;
         switch (typeOfTheArray) {
             case "random":
@@ -233,5 +208,5 @@ public class ArrayGeneratorGeneral {
         }
 
         return result;
-    }   
+    }
 }
